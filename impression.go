aa91c41b68320aab9bffe28a0dc411e5d75fa679
Johnathan Butler <johnathan.butler@unity3d.com>
2025-11-18 10:22:18 -0800

    [#ABT-3931] Remove omitempty from ambiguous regs.gdpr, imp.instl (kind of)

--- impression.go
+++ impression.go
@@ -26,7 +26,7 @@ type Impression struct {
 	DisplayManager    string         `json:"displaymanager,omitempty"`    // Name of ad mediation partner, SDK technology, etc
 	DisplayManagerVer string         `json:"displaymanagerver,omitempty"` // Version of the above
 	Clickbrowser      int            `json:"clickbrowser"`                // Indicates the type of browser opened upon clicking the creative in an app, where 0 = embedded, 1 = native
-	Instl             int            `json:"instl"`                       // Interstitial, Default: 0 ("1": Interstitial, "0": Something else)
+	Instl             int            `json:"instl,omitempty"`             // Interstitial, Default: 0 ("1": Interstitial, "0": Something else)
 	TagID             string         `json:"tagid,omitempty"`             // IDentifier for specific ad placement or ad tag
 	BidFloor          float64        `json:"bidfloor,omitempty"`          // Bid floor for this impression in CPM
 	BidFloorCurrency  string         `json:"bidfloorcur,omitempty"`       // Currency of bid floor
